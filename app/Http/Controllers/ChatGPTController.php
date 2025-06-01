d2181663e3a1a54796570794126401dd46153a21
jared <jared.sotoperez98@gmail.com>
2025-06-01 19:07:25 -0400

    ss

--- app/Http/Controllers/ChatGPTController.php
+++ app/Http/Controllers/ChatGPTController.php
@@ -5,27 +5,7 @@ use Illuminate\Support\Arr;
 use OpenAI\Laravel\Facades\OpenAI; 
 class ChatGPTController extends Controller 
 { 
-/** 
-* Write code on Method 
-* 
-* @return response() 
-*/ 
-// public function index(Request $request) 
-// { 
-// $result = ''; 
-// if ($request->filled('title')) { 
-// $messages = [ 
-// ['role' => 'user', 'content' => $request->title], 
-// ]; 
-// $result = OpenAI::chat()->create([ 
-// 'model' => 'gpt-4.1-nano', 
-// 'messages' => $messages, 
-// ]); 
-// $result = Arr::get($result, 'choices.0.message')['content'] ?? ''; 
-// } 
-// return view('chatGPT', compact('result')); 
-// } 
-// }
+
 public function index()
 {
     \Log::info('ENTRO A CHAT-GPT');
