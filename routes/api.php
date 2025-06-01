e59f49b46bdaab5f84ac77364b1ea9b6c9342a42
jared <jared.sotoperez98@gmail.com>
2025-06-01 19:00:09 -0400

    testt

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\ChatGPTController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/chat-gpt', [ChatGPTController::class, 'index']);
+Route::get('/ping', fn() => response()->json(['pong' => true]));
